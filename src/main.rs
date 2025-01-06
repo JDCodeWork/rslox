4d19d3ed0f32f9b3bbd66f99917397ed6b5c6c45
JDCodeWork <jdorozco792006@gmail.com>
2025-01-05 21:14:29 -0500

    Support for file reading
    Added support for reading from path given by console

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::{env, fs};
+
+fn run(raw_code: String) {
+    println!("Your code is\n\n{}", raw_code)
+}
+
+fn run_file(path: String){
+    let raw_code = fs::read_to_string(path).expect("Failed to read the file");
+    run(raw_code);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut args: Vec<String> = env::args().collect();
+
+    if args.len() == 1 {
+        println!("Run Interactive CLI")
+    } else if args.len() == 2 {
+        run_file(args.remove(1));
+    } else {
+        println!("Usage: rslox <file_path>")
+    }
 }
